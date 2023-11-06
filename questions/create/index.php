1b91f79750b2b3adf3b4deff944a477211e4607e
Adrian-Tsai <5lordadrian2004@gmail.com>
2023-11-06 23:42:44 +0630

    Questions create form

--- questions/create/index.php
+++ questions/create/index.php
@@ -13,8 +13,21 @@ require($nav);
     <div class="container">
 
         <div class="create-post-container">
+            <h1>Ask A Question:</h1>
             <form class="create-post-form" action="" method="POST" enctype="multipart/form-data">
-                <p>Test. Test. Test.</p>
+                <div class="textbox">
+                    <label for="title">Title</label>
+                    <input type="text" id="title">
+                </div>
+                <div class="textbox">
+                    <label for="content">Content</label>                    
+                    <textarea name="" id="" cols="30" rows="10"></textarea>
+                </div>
+                <span class="buttons">
+                    <a href="/questions/" class="btn-main btn-cancel">Cancel</a>
+                    <button class="btn-main" id="submit" type="submit">Submit</button>
+                </span>
+                
             </form>
         </div>
 
