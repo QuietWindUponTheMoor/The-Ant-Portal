298e00febab04995ae1298dff14aff675c6f150e
Quiet Wind Upon The Moor <calebhesse42@gmail.com>
2023-11-07 14:25:49 -0600

    Profile changes!

--- users/user.php
+++ users/user.php
@@ -8,6 +8,10 @@ $footer .= "/includes/footer.php";
 require($head);
 require($nav);
 
+if (!isset($_GET["userID"]) || $_GET["userID"] < 1) {
+    header("Location: /");
+}
+
 if ($isLoggedIn === true && isset($_GET["userID"]) && $_GET["userID"] == $userID) {
     // If the current user is the owner of this profile
     $isOwner = true;
@@ -68,7 +72,79 @@ require($forms);
                 <div class="feed-sub">
 
 
-                    <div class="post">
+                    <?php
+                    $res = $db->select("SELECT * FROM posts WHERE userID=? ORDER BY postID DESC;", "i", $_GET["userID"]);
+                    if ($res->num_rows > 0) {
+                        while ($d = mysqli_fetch_assoc($res)) {
+                            $fromUserID = $d["userID"];
+                            $type = $d["type"];
+                            $title = $d["title"];
+                            $body = $d["text"];
+                            $views = $d["views"];
+                            $upvotes = $d["upvotes"];
+                            $downvotes = $d["downvotes"];
+                            $datetime = $d["datetime"];
+                            $tagOne = $d["tagOne"];
+                            $tagTwo = $d["tagTwo"];
+                            $tagThree = $d["tagThree"];
+                            $tagFour = $d["tagFour"];
+                            $tagFive = $d["tagFive"];
+                    
+                            if ($type === 1) {
+                                $type = "Question";
+                            } else if ($type === 2) {
+                                $type = "Sighting";
+                            } else if ($type === 3) {
+                                $type = "Nuptial Flight";
+                            } else {
+                                $type = $type;
+                            }
+                            $userdata = getUserData($_GET["userID"], $db);
+                            $username = $userdata[0];
+                            $userImage = $userdata[1];
+                    
+                            echo
+                            '
+                            <div class="post">
+                                <div class="content">
+                                    <a class="post-title ellipsis" href="#">'.$title.'</a>
+                                    <p class="text-preview twoline-ellipsis">'.$body.'</p>
+                                </div>
+                                <div class="meta">
+                                    <p class="meta-info" id="post-type">'.$type.'</p>
+                                </div>
+                                <div class="meta">
+                                    <p class="meta-info" id="upvotes">'.$upvotes.' upvotes</p>
+                                    <p class="meta-info" id="downvotes">'.$downvotes.' downvotes</p>
+                                    <p class="meta-info" id="answers">? answers</p>
+                                    <p class="meta-info" id="views">'.$views.' views</p>
+                                </div>
+
+                                <div class="meta">
+                                    <div class="user">
+                                        <div class="user-image-container"><img class="user-image" src="'.$userImage.'"/></div>
+                                        <a class="username" href="/users/user?userID='.$fromUserID.'">'.$username.'</a>
+                                        <p class="time">posted on '.$datetime.'</p>
+                                    </div>
+                                </div>
+                            </div>
+                            ';
+                        }
+                    }
+                    
+                    function getUserData($userID, $db) {
+                        $res = $db->select("SELECT * FROM users WHERE userID=?;", "i", $userID);
+                        if ($res->num_rows > 0) {
+                            $d = mysqli_fetch_assoc($res);
+                            $username = $d["username"];
+                            $image = $d["image"];
+                            return [$username, $image];
+                        }
+                    }
+                    ?>
+
+
+                    <!--<div class="post">
                         <div class="content">
                             <a class="post-title ellipsis" href="#">This is an example post.</a>
                             <p class="text-preview twoline-ellipsis">Here's an example of a post's text content preview.</p>
@@ -150,7 +226,7 @@ require($forms);
                                 <p class="time">posted on ?</p>
                             </div></a>
                         </div>
-                    </div>
+                    </div>-->
 
 
                 </div>
