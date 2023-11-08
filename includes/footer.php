b6fcadf7f3651283640ac5a26f34cabfd64c7dd0
QuietWind01 <calebhesse42@gmail.com>
2023-11-07 23:07:03 -0600

    Added stuff to footer, done for the time being

--- includes/footer.php
+++ includes/footer.php
@@ -14,26 +14,14 @@
         </div>
 
         <div class="footer-section">
-            <div class="item-title">Socials</div>
+            <div class="item-title">Site</div>
             <div class="item">
-                <div class="item-image-container"><img class="item-image" src="/web_images/icons/logo-facebookpng-32204.png"/></div>
-                <a class="item-content" href="https://www.facebook.com/profile.php?id=61552957816126">The Ant Portal</a>
+                <div class="item-image-container"><img class="item-image" src="/web_images/icons/favicon.ico"/></div>
+                <a class="item-content unfinished" href="#">API</a>
             </div>
             <div class="item">
-                <div class="item-image-container"><img class="item-image" src="/web_images/icons/google-logo-9808.png"/></div>
-                <a class="item-content unfinished" href="h">The Ant Portal</a>
-            </div>
-        </div>
-
-        <div class="footer-section">
-            <div class="item-title">Socials</div>
-            <div class="item">
-                <div class="item-image-container"><img class="item-image" src="/web_images/icons/logo-facebookpng-32204.png"/></div>
-                <a class="item-content" href="https://www.facebook.com/profile.php?id=61552957816126">The Ant Portal</a>
-            </div>
-            <div class="item">
-                <div class="item-image-container"><img class="item-image" src="/web_images/icons/google-logo-9808.png"/></div>
-                <a class="item-content unfinished" href="h">The Ant Portal</a>
+                <div class="item-image-container"><img class="item-image" src="/web_images/icons/favicon.ico"/></div>
+                <a class="item-content unfinished" href="#">API Documentation</a>
             </div>
         </div>
 
