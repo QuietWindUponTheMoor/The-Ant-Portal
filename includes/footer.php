b35cb22709afee3e6a92188258bb2a80df2d2018
QuietWind01 <calebhesse42@gmail.com>
2023-11-07 23:02:58 -0600

    Started footer + cosmetic work

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,42 @@
 <div class="footer">
     <div class="footer-sub">
-        
+    
+        <div class="footer-section">
+            <div class="item-title">Socials</div>
+            <div class="item">
+                <div class="item-image-container"><img class="item-image" src="/web_images/icons/logo-facebookpng-32204.png"/></div>
+                <a class="item-content" href="https://www.facebook.com/profile.php?id=61552957816126">The Ant Portal</a>
+            </div>
+            <div class="item">
+                <div class="item-image-container"><img class="item-image" src="/web_images/icons/google-logo-9808.png"/></div>
+                <a class="item-content unfinished" href="h">The Ant Portal</a>
+            </div>
+        </div>
+
+        <div class="footer-section">
+            <div class="item-title">Socials</div>
+            <div class="item">
+                <div class="item-image-container"><img class="item-image" src="/web_images/icons/logo-facebookpng-32204.png"/></div>
+                <a class="item-content" href="https://www.facebook.com/profile.php?id=61552957816126">The Ant Portal</a>
+            </div>
+            <div class="item">
+                <div class="item-image-container"><img class="item-image" src="/web_images/icons/google-logo-9808.png"/></div>
+                <a class="item-content unfinished" href="h">The Ant Portal</a>
+            </div>
+        </div>
+
+        <div class="footer-section">
+            <div class="item-title">Socials</div>
+            <div class="item">
+                <div class="item-image-container"><img class="item-image" src="/web_images/icons/logo-facebookpng-32204.png"/></div>
+                <a class="item-content" href="https://www.facebook.com/profile.php?id=61552957816126">The Ant Portal</a>
+            </div>
+            <div class="item">
+                <div class="item-image-container"><img class="item-image" src="/web_images/icons/google-logo-9808.png"/></div>
+                <a class="item-content unfinished" href="h">The Ant Portal</a>
+            </div>
+        </div>
+
     </div>
 </div>
 
