325cbeb01413848e85b9f63f1bc6266ffdb036d8
QuietWind01 <calebhesse42@gmail.com>
2023-11-30 18:34:13 -0600

    Converted some JS to TypeScript

--- includes/footer.php
+++ includes/footer.php
@@ -5,11 +5,11 @@
             <div class="item-title">Socials</div>
             <div class="item">
                 <div class="item-image-container"><img class="item-image" src="/web_images/icons/logo-facebookpng-32204.png"/></div>
-                <a class="item-content" href="https://www.facebook.com/profile.php?id=61552957816126">The Ant Portal</a>
+                <a class="item-content" href="https://www.facebook.com/profile.php?id=61552957816126" target="__blank">The Ant Portal</a>
             </div>
             <div class="item">
                 <div class="item-image-container"><img class="item-image" src="/web_images/icons/google-logo-9808.png"/></div>
-                <a class="item-content unfinished" href="h">The Ant Portal</a>
+                <a class="item-content unfinished" href="h" target="__blank">The Ant Portal</a>
             </div>
         </div>
 
@@ -17,11 +17,11 @@
             <div class="item-title">Site</div>
             <div class="item">
                 <div class="item-image-container"><img class="item-image" src="/web_images/icons/favicon.ico"/></div>
-                <a class="item-content unfinished" href="#">API</a>
+                <a class="item-content unfinished" href="#" target="__blank">API</a>
             </div>
             <div class="item">
                 <div class="item-image-container"><img class="item-image" src="/web_images/icons/favicon.ico"/></div>
-                <a class="item-content unfinished" href="#">API Documentation</a>
+                <a class="item-content unfinished" href="#" target="__blank">API Documentation</a>
             </div>
         </div>
 
--- nup_flight/index.php
+++ nup_flight/index.php
@@ -42,7 +42,7 @@ if (!isset($_GET["flightID"]) || $_GET["flightID"] < 1) {
         $totalVotes = $upvotes - $downvotes;
         // Vote count colors:
         if ($totalVotes < 0) {
-            $color = "success";
+            $color = "error";
         } else if ($totalVotes > 0 && $totalVotes < 5) {
             $color = "warning";
         } else if ($totalVotes >= 5) {
@@ -132,9 +132,9 @@ $replyType = "post";
                     </div>
                     <div class="post-section vote-container">
                         <div class="vote-subcontainer">
-                            <div class="icon-container" title="Upvote this post"><img class="icon" id="upvote-trigger" style="<?php echo $upvoteButtonOpacity; ?>" src="/web_images/icons/upvote.png"/></div>
+                            <div class="icon-container" title="Upvote this post"><img class="icon vote-image" id="upvote-trigger" style="<?php echo $upvoteButtonOpacity; ?>" src="/web_images/icons/upvote.png"/></div>
                             <p class="vote-count <?php echo $color; ?>" id="total-votes"><?php echo $totalVotes; ?></p>
-                            <div class="icon-container" title="Downvote this post"><img class="icon" id="downvote-trigger" style="<?php echo $downvoteButtonOpacity; ?>" src="/web_images/icons/downvote.png"/></div>
+                            <div class="icon-container" title="Downvote this post"><img class="icon vote-image" id="downvote-trigger" style="<?php echo $downvoteButtonOpacity; ?>" src="/web_images/icons/downvote.png"/></div>
                         </div>
                     </div>
                     <div class="post-section body-container">
@@ -265,6 +265,7 @@ $replyType = "post";
 // Get backend data
 // User
 const $isLoggedIn = <?php if ($isLoggedIn === false) {echo false;} else {echo true;} ?>;
+const $flaggingModal = `<?php echo $flagging_modal; ?>`;
 const user_data = {
     id: <?php echo $userID; ?>,
     username: "<?php echo $thisUsersUsername; ?>",
@@ -275,293 +276,7 @@ const post_data = {
     postType: 4, // 4 = nuptial_flight
     postID: <?php echo $flightID; ?>,
 };
-
-
-
-// Replies
-// Initialize current_val:
-let current_textbox_val = "";
-async function submitReply(forType, $this) {
-    // Prevent default actions
-    event.preventDefault();
-
-    // Get comment thread
-    $comment_thread = $this.closest(".comments-box-main").find(".comments-list");
-    // Get textbox
-    $textbox = $this.closest(".textarea-container").find(".comment-textbox");
-
-    // Fetch reply text
-    const reply_text = $textbox.val();
-
-    // Get user's date
-    const userDate = new Date();
-    const options = { timeZone: 'America/Chicago' };
-    const month = (userDate.getMonth() + 1).toString().padStart(2, '0');
-    const day = userDate.getDate().toString().padStart(2, '0');
-    const year = userDate.getFullYear();
-    const hours = userDate.getHours() % 12 || 12;
-    const minutes = userDate.getMinutes().toString().padStart(2, '0');
-    const period = userDate.getHours() < 12 ? 'AM' : 'PM';
-    const timeZone = new Intl.DateTimeFormat('en-US', { timeZoneName: 'short', timeZone: options.timeZone }).format(userDate).split(' ').pop();
-    const timeString = `${month}/${day}/${year} @ ${hours}:${minutes}${period} (${timeZone})`;
-
-    // Initialize reply template
-    const reply_template =
-    `
-    <div class="comment-container">
-        <div class="comment-options post-flagging">
-            <?php echo $flagging_modal; ?>
-        </div>
-        <div class="comment-content">
-            <a class="user-link" href="/users/user?userID=<?php echo $userID; ?>"><?php echo $thisUsersUsername; ?></a>
-            <p class="comment">${reply_text}</p>
-            <p class="comment comment_date">${timeString}</p>
-        </div>
-    </div>
-    `;
-    
-    // Get reply data
-    const reply_data = {
-        db: "<?php echo $dbHost; ?>",
-        userID: <?php echo $userID; ?>,
-        forType: forType,
-        itemID: <?php echo $flightID; ?>,
-        text: reply_text,
-        datetime: timeString,
-    };
-
-    // Submit the reply
-    await $.ajax({  
-        type: "POST",  
-        url: "/php/lib/replies/create_reply.php", 
-        data: reply_data,
-        success: function (response) {
-            response = parseInt(response);
-            if (response === 1) {
-                $comment_thread.append(reply_template);
-                $textbox.val("");
-                // Find the parent of the textbox with class 'textarea-container'
-                $textarea_container = $textbox.closest(".textarea-container");
-                // Reverse the classes for it
-                $textarea_container.addClass("textarea-container-inactive").removeClass("textarea-container-active");
-                // Textbox placeholder change
-                $textbox.attr("placeholder", "Add Comment");
-                // Reset current_textbox_val
-                current_textbox_val = "";
-            } else {
-                // Save the current tetbox value just in case
-                current_textbox_val = $textbox.val();
-                // Display error to user via textbox
-                $textbox.val(response);
-            }
-        }
-    });
-}
-
-// Reply textboxes
-$(".comment-textbox").on("input", function () {
-    $(this).css("height", "auto").css("height", this.scrollHeight + "px");
-});
-$(".textarea-container").on("click", function (event) {
-    // Prevent click propagation
-    event.stopPropagation();
-
-    // Add active class, remove inactive class
-    $(this).addClass("textarea-container-active").removeClass("textarea-container-inactive");
-
-    // Assign $this variable as $(this) to target a specific element later
-    $this = $(this);
-    
-    // Get current textbox
-    $textbox = $this.find(".comment-textbox");
-
-    // Textbox placeholder change
-    $textbox.attr("placeholder", "Write your comment here. Please refrain from needless comments.");
-
-    if ($textbox.val()) {
-        // Textbox has text in it
-        // Save the current text in the box:
-        current_textbox_val = $textbox.val();
-    } else {
-        // Textbox does not have text in it
-        // Re-assign the old text if there was any
-        $textbox.val(current_textbox_val);
-        // Assign current textbox value
-        current_textbox_val = $textbox.val();
-    }
-
-    // If the above is already done, and the user clicks outside of this element, reverse the classes
-    $(document).on("click", () => {
-        // Reverse the classes to original
-        $this.addClass("textarea-container-inactive").removeClass("textarea-container-active");
-        // Textbox placeholder change
-        $textbox.attr("placeholder", "Add Comment");
-
-        if ($textbox.val()) {
-            // Textbox has text in it
-            // Save the current text in the box:
-            current_textbox_val = $textbox.val();
-        }
-        
-        // Remove the text from the textbox
-        $textbox.val("");
-    });
-});
-
-// Subitting flags
-const flagging_response = $("#flagging-response");
-let $radioContainers = $(".radio-section");
-$("#flagging-form").on("submit", async (event) => {
-    // Prevent default actions
-    event.preventDefault();
-    // Get form data
-    const formData = new FormData(document.getElementById("flagging-form"));
-
-    // Submit the form
-    await sendAJAX("/php/lib/flagging/submit_report.php", formData, "POST", false, false, function (unparsedResponse) {
-        const response = JSON.parse(unparsedResponse);
-        const resCode = response.code;
-        const reason = response.reason;
-        const link = response.reported_from;
-        const explanation = response.report_explanation;
-        const success = response.success;
-        const error = response.error;
-        if (resCode === 1) {
-            // Success
-            document.getElementById("flagging-form").reset();
-            $radioContainers.hide();
-            $("#submit-flagging").hide();
-            $(".explanation-section").hide();
-            $("#cancel-flagging").text("Finished");
-            flagging_response.html(success).addClass("success");
-        } else {
-            // Error
-            flagging_response.html(error).addClass("error");
-        }
-    });
-});
-
-
-// Initial widths of flagging modal
-let sectionWidth = $(".post-flagging").width();
-$(".flagging-modal").css("width", Math.floor(sectionWidth)).css("max-width", Math.floor(sectionWidth) + "px");
-// Dynamic widths
-$(window).on("resize", () => {
-    let sectionWidth = $(".post-flagging").width();
-    $(".flagging-modal").css("width", Math.floor(sectionWidth)).css("max-width", Math.floor(sectionWidth) + "px");
-});
-
-// Selected flagging reasons controls
-$radioContainers.find(":radio").on("change", e => {
-    $radioContainers.removeClass("radio-active"); // remove from all containers
-    $(e.target).closest(".radio-section").addClass("radio-active"); // add class to current
-});
-
-// Opening/closing the flagging modal
-$(".flag-image").on("click", function () {
-    let modal = $(this).next(".flagging-modal");
-
-    // Apply styles to the modal or flag image
-    modal.css("display", "flex").hide().fadeIn();
-});
-$(".cancel-flagging").on("click", () => {
-    $(".flagging-modal").hide();
-});
-
-// Change image color to red upon hover of flag button
-$(".flag-image").on("mouseenter", function () {
-    $(this).attr("src", "/web_images/icons/flag_red.png");
-});
-$(".flag-image").on("mouseleave", function () {
-    $(this).attr("src", "/web_images/icons/flag.png");
-});
-
-// Upvote triggers
-$("#upvote-trigger").on("click", async () => {
-    if ($isLoggedIn === 1) {
-        await upvote();
-    } else {
-        alert("Sorry, but you must be logged in to vote.");
-    }
-});
-$("#downvote-trigger").on("click", async () => {
-    if ($isLoggedIn === 1) {
-        await downvote();
-    } else {
-        alert("Sorry, but you must be logged in to vote.");
-    }
-});
-
-if ($isLoggedIn !== 1) {
-    $(".post-flagging").hide();
-    $(".comment-box-write").hide();
-}
-
-// Helpers
-async function upvote() {
-    $("#upvote-trigger").css("opacity", "0.6");
-    $("#downvote-trigger").css("opacity", "0.6");
-    $.ajax({  
-        type: "POST",  
-        url: "/php/lib/voting/post_voting.php", 
-        data: {
-            voteType: 2,
-            postID: <?php echo $flightID; ?>,
-            userID: <?php echo $userID; ?>,
-            upvoteOrDownvote: 1
-        },
-        success: function(response) {
-            // ParseInt--if parseInt DOESN'T result in "NaN", then it was a success.
-            if (response === NaN) {
-                // Not successful
-                alert(response);
-            } else if (response !== NaN) {
-                $("#total-votes").text(response);
-                $("#upvote-trigger").css("opacity", "1");
-            } else {
-                alert(response);
-            }
-        }
-    });
-}
-async function downvote() {
-    $("#upvote-trigger").css("opacity", "0.6");
-    $("#downvote-trigger").css("opacity", "0.6");
-    $.ajax({  
-        type: "POST",  
-        url: "/php/lib/voting/post_voting.php", 
-        data: {
-            voteType: 2,
-            postID: <?php echo $flightID; ?>,
-            userID: <?php echo $userID; ?>,
-            upvoteOrDownvote: 2
-        },
-        success: function(response) {
-            // ParseInt--if parseInt DOESN'T result in "NaN", then it was a success.
-            if (response === NaN) {
-                // Not successful
-                alert(response);
-            } else if (response !== NaN) {
-                $("#total-votes").text(response);
-                $("#downvote-trigger").css("opacity", "1");
-            } else {
-                alert(response);
-            }
-        }
-    });
-}
-async function sendAJAX(url, dataObj, method, processData, contentType, __callback) {
-    // Execute AJAX
-    await $.ajax({  
-        type: method,  
-        url: url, 
-        data: dataObj,
-        processData: processData,
-        contentType: contentType,
-        success: __callback
-    });
-}
 </script>
-<script type="text/javascript" src="/js/lib/editing/editing.js"></script>
+<script type="text/javascript" src="/js/lib/page_control/posts.js"></script>
 
 <?php require($root."/includes/footer.php"); ?>
