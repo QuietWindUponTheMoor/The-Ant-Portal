afc40997a628b8c7d44a120da9bea650807febaa
QuietWind01 <calebhesse42@gmail.com>
2023-12-16 23:41:54 -0600

    Refactored navbar layout

---
+++ includes/navbar.php
@@ -0,0 +1,15 @@
+<div class="nav col">
+    <div class="nav-sub row">
+        <div class="main row">
+            <a class="site-title nav-hover-animation" href="/">The Ant Portal</a>
+            <a class="button nav-hover-animation" href="#">Btn 1</a>
+            <a class="button nav-hover-animation" href="#">Btn 2</a>
+            <a class="button nav-hover-animation" href="#">Btn 3</a>
+        </div>
+        <div class="secondary row">
+            <a class="button nav-hover-animation" href="#">Btn 1</a>
+            <a class="button nav-hover-animation" href="#">Btn 2</a>
+            <a class="button nav-hover-animation" href="#">Btn 3</a>
+        </div>
+    </div>
+</div>
